96e7061e91fbdf76726df413167c5e4774b66958
Protocolelectronicsco <Protocolelectronicsco@gmail.com>
2023-08-09 14:10:59 -0500

    Primeras rutas

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import './App.css';
 import  InputTexto  from './components/InputTexto';
 import React from 'react';
-const Home = () => <h1>Home</h1>
-const User = () => <h1>user</h1>
 
-const Minombre: string = "Mi nombre"
 function App() {
   return (
     <div className="App">
