ca4c536ad69b88898256c5758762d452b13026ec
Protocolelectronicsco <Protocolelectronicsco@gmail.com>
2023-08-14 22:12:41 -0500

    Modificacion en Analisis Fisico y Catacion

--- src/components/AnalisisFisico.tsx
+++ src/components/AnalisisFisico.tsx
@@ -1,19 +1,41 @@
 //import { useState } from "react";
 import { useLocation, } from "react-router-dom";
+//import AnalisisFisicoLote from "./AnalisisFisicoLote";
+//(import '.../public/css/registerCaficultor.css';
+//<AnalisisFisicoLote {...codigo:"w4e234"; variedad:Variedad; altura:1800; proceso:"Lavado"}/>
+function AnalisisFisicoLote ({codigo, variedad, altura, proceso, municipio}:{codigo:string, variedad:string, altura:number, proceso:string, municipio:string}) {
+    //export default function AnalisisFisicoLote  ({codigo, altura}:{codigo:string, altura:number}) {
+    console.log("Entre a la funcion")
+    return (
+            <div>
+                <body>
 
+                    <h2>Codigo del lote {codigo}</h2>
+                    <h2>Nombre de caficultor: {variedad}</h2>
+                    <h2>Municipio de cultivo: {municipio}</h2>
+                    <h2>Variedad de cultivo: {variedad}</h2>
+                    <h1> Este es el análisis fisico</h1>
+                </body>
+            </div>
+    )}
 export default function AnalisisFisico ()  {
-   
     const location= useLocation()
     const { data } = location.state || {};
-    const {ID, NombreCaficultor,  Municipio, Variedad} = data || {};
-    //const {data} = useLocation()
+    const {ID, NombreCaficultor,  Municipio, Variedad, AlturaLote, ProcesoLote}:{ID:string, NombreCaficultor:string,  Municipio:string, Variedad:string, AlturaLote:number, ProcesoLote:string} = data || {};
+    //const {data} = useLocation()                      
+    
+        //<AnalisisFisicoLote  codigo={ID} variedad = {Variedad} municipio ={Municipio} altura={AlturaLote} proceso ={ProcesoLote} />
     return(
-       <div>
-           <h2>Codigo del lote {ID}</h2>
-           <h2>Nombre de caficultor: {NombreCaficultor}</h2>
-           <h2>Municipio de cultivo: {Municipio}</h2>
-           <h2>Variedad de cultivo: {Variedad}</h2>
-           <h1> Este es el análisis fisico</h1>
+       <div className="Inputs">
+           <body>
+            <button onClick={()=>AnalisisFisicoLote({
+                codigo:ID,
+                variedad:Variedad,
+                municipio:Municipio,
+                altura:AlturaLote,
+                proceso:ProcesoLote
+            })}>Nuevo Análisis</button>
+           </body>
         </div>
     )
 }
--- src/components/Catacion.tsx
+++ src/components/Catacion.tsx
@@ -1,6 +1,35 @@
 import { useLocation, } from "react-router-dom";
+import React, { useState } from 'react';
 
+type defecto= {
+    id: number;
+    nombre: string;
+    porcentaje: number;
+}
+type puntajeSCA = 7 | 7.25 | 7.5 | 7.75 | 8 | 8.25 | 8.5 | 8.75 | 9 | 9.25 | 9.5 | 9.75 | 10
+interface CatacionData {
+    codigo: string;
+    variedad: string;
+    altura: number;
+    proceso: string;
+    factordeRendimiento: number;
+    defectos: defecto[];
+    tostion: number;
+    catador: string;
+    fragancia: puntajeSCA
+}
 export default function Catacion ()  {
+    const [catacionCount, setCatacionCount] = useState(0);
+    const [catacionElements, seCatacionElements] = useState<JSX.Element[]>([]);
+
+    const handleButtonClick = () => {
+        const newCatacionCount = catacionCount + 1;
+        setCatacionCount(newCatacionCount);
+
+        const newInputElement = <input key={newCatacionCount} type="text" placeholder={`Input ${newInputCount}`} />;
+        setInputElements(prevElements => [...prevElements, newInputElement]);
+    };
+
    
     const location= useLocation()
     const { data } = location.state || {};
@@ -12,6 +41,11 @@ export default function Catacion ()  {
            <h2>Nombre de caficultor: {NombreCaficultor}</h2>
            <h2>Municipio de cultivo: {Municipio}</h2>
            <h1> Esta es la pagina de catacion</h1>
+           <div>
+                <h1>Botón para Agregar Inputs</h1>
+                <button onClick={handleButtonClick}>Agregar Input</button>
+                {inputElements.map(inputElement => inputElement)}
+            </div>
         </div>
     )
 }
