e5cbf185348430709d9162b3c45d5028a07e5ac4
Isaac Chaves <ichavesgomez10@gmail.com>
2023-08-29 14:46:48 -0500

    archivos de prueba

--- server/index.js
+++ server/index.js
@@ -1,11 +1,8 @@
-require("dotenv").config(); // Agrega esto al principio de tu archivo server/index.js
+require("dotenv"); // Agrega esto al principio de tu archivo server/index.js
 const express = require("express");
 const bodyParser = require("body-parser");
 const pino = require("express-pino-logger")();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = process.env.authToken;
-const client = require("twilio")(accountSid, authToken);
-require('dotenv');
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -15,30 +12,26 @@ app.get("/api/greeting/:name", (req, res) => {
   const name = req.query.name || req.params.name;
   res.setHeader("Content-Type", "application/json");
   res.send(JSON.stringify({ greeting: `Hello ${name}!` }));
+
 });
 
 app.listen(3001, () =>
   console.log("Express server is running on localhost:3001")
 );
 
-app.post("/api/enviar-catacion", async (req, res) => {
-  const valoresCatacion = req.body;
 
-  // Envía los datos de la catazon a WhatsApp
-  const client = require("twilio")(
-    process.env.TWILIO_ACCOUNT_SID,
-    process.env.TWILIO_AUTH_TOKEN,
-    {
-      password: process.env.authToken,  
-    }
-  );
 
-  const message = client.messages.create({
-    body: JSON.stringify(valoresCatacion),
-    to: "whatsapp:+573166313394",
-  });
+  // const client = require("twilio")(
+  //   "ACb55a91863b1edc341d35b33984b2f908",
+  //   "ca86ec5eb1c7cc0d02f95bb2a1cdf99a"
+  // );
+  // const mivariable = "hola mundo";
+  // client.messages
+  //   .create({
+  //     body: mivariable,
+  //     from: "whatsapp:+14155238886",
+  //     to: "whatsapp:+573166313394",
+  //   })
+  //   .then((message) => console.log(message.sid));
 
-  console.log(message);
-
-  res.send(200);
-});
+  // ESTA FUNCION ES LA QUE MANDA EL MENSAJE A WHATSAPP, COMENTADA PARA QUE NO SE EJECUTE CADA QUE SE INICIE EL PROYECTO Y NO GASTAR LOS CREDITOS DE PUREBA
